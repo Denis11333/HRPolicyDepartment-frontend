2e725cec1a96b6ef89440f6971e4227d47daa221
Denis <77487610+Denis11333@users.noreply.github.com>
2023-11-10 08:17:57 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {LoginPageComponent} from './pages/login-page/login-page.component';
-import {RegistrationPageComponent} from './pages/registration-page/registration-page.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {provideToastr, ToastrModule} from "ngx-toastr";
-import {provideAnimations, BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NavigationComponent} from './components/navigation/navigation.component';
-import {SliderComponent} from './components/slider/slider.component';
-import {MainPageComponent} from './pages/main-page/main-page.component';
-import {CarouselModule} from '@coreui/angular';
-import {NgOptimizedImage} from "@angular/common";
-import {AboutPageComponent} from './pages/about-page/about-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideToastr, ToastrModule } from 'ngx-toastr';
+import { provideAnimations, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { CarouselModule } from '@coreui/angular';
+import { NgOptimizedImage } from '@angular/common';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { FooterComponent } from './components/footer/footer.component';
 import { VacancyComponent } from './components/vacancy/vacancy.component';
-import { VacanciesPageComponent } from './pages/vacancies-page/vacancies-page.component'
+import { VacanciesPageComponent } from './pages/vacancies-page/vacancies-page.component';
 import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
 import { VacancyDetailsPageComponent } from './pages/vacancy-details-page/vacancy-details-page.component';
 import { VacancyDetailsComponent } from './components/vacancy-details/vacancy-details.component';
 import { MyVacanciesPageComponent } from './pages/my-vacancies-page/my-vacancies-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MyVacancyModalComponent } from './components/modals/my-vacancy-modal/my-vacancy-modal.component';
 
 
 @NgModule({
@@ -42,6 +43,7 @@ import { HttpClientModule } from '@angular/common/http';
     VacancyDetailsPageComponent,
     VacancyDetailsComponent,
     MyVacanciesPageComponent,
+    MyVacancyModalComponent
   ],
   imports: [
     BrowserModule,
@@ -62,10 +64,10 @@ import { HttpClientModule } from '@angular/common/http';
       timeOut: 5000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
-      enableHtml: true
+      enableHtml: true,
     }),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
---
+++ src/app/components/modals/my-vacancy-modal/my-vacancy-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-vacancy-modal',
+  templateUrl: './my-vacancy-modal.component.html',
+  styleUrls: ['./my-vacancy-modal.component.scss']
+})
+export class MyVacancyModalComponent {
+}
